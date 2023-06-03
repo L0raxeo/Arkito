14a5b0ae61140be8b57f0ece469d78e7f8b62c88
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2023-06-03 14:59:16 -0400

    Sample Scene

--- src/main/java/arkiGame/components/SampleComponent.java
+++ src/main/java/arkiGame/components/SampleComponent.java
@@ -1,7 +1,11 @@
 package arkiGame.components;
 
+import l0raxeo.arki.engine.assetFiles.AssetPool;
+import l0raxeo.arki.engine.audio.AudioManager;
 import l0raxeo.arki.engine.components.Component;
 import l0raxeo.arki.engine.components.collisionComponents.RigidBody;
+import l0raxeo.arki.engine.eventSystem.EventHandler;
+import l0raxeo.arki.engine.eventSystem.EventTrigger;
 import l0raxeo.arki.engine.input.mouse.MouseManager;
 import l0raxeo.arki.engine.scenes.Camera;
 import org.joml.Vector2f;
@@ -9,10 +13,21 @@ import org.joml.Vector2f;
 public class SampleComponent extends Component
 {
 
+    private EventTrigger onClickTrigger = new EventTrigger("test");
+
+    @Override
+    public void start()
+    {
+        EventHandler.getEventTrigger("test").subscribe(this, "onClick");
+    }
+
     @Override
     public void update(double dt) {
         if (MouseManager.hasPressedInput())
+        {
             gameObject.getComponent(RigidBody.class).addForce(new Vector2f(0, -10));
+            onClickTrigger.triggerEvent(this);
+        }
 
         gameObject.transform.rotation = Camera.getDegFromOriginToTarget(
                 (int) gameObject.transform.getScreenCenterPosition().x,
@@ -20,6 +35,11 @@ public class SampleComponent extends Component
                 MouseManager.getScreenMouseX(),
                 MouseManager.getScreenMouseY()
         );
+    }
+
+    public void onClick()
+    {
+        AudioManager.play(AssetPool.getAudioClip("sample_audio", "assets/samples/audios/sample_audio.wav", 0));
     }
 
 }
